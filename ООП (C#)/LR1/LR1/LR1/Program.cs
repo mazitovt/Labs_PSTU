8d6e4fd5ed4bbb05d67a4372d3deb8fb62937c3b
Timur Mazitov <65964304+mazitovt@users.noreply.github.com>
2021-07-13 20:23:56 +0500

    Add files via upload

---
+++ ООП (C#)/LR1/LR1/LR1/Program.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace Lab1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Task();
+        }
+
+        static void Read(ref float m, ref float n)
+        {
+            bool flag;
+            do
+            {
+                Console.WriteLine("Введите число m: ");
+                string buff = Console.ReadLine();
+                buff = buff.Replace(".", ",");
+                flag = float.TryParse(buff, out m);
+                Console.WriteLine("Введите число n: ");
+                buff = Console.ReadLine();
+                buff = buff.Replace(".", ",");
+                flag &= float.TryParse(buff, out n);
+            } while (!flag);
+        }
+
+        static void Read_x(ref double x)
+        {
+            bool flag;
+            do
+            {
+                Console.WriteLine("Введите число x [-1;INF): ");
+                string buff = Console.ReadLine();
+                buff = buff.Replace(".", ",");
+                flag = double.TryParse(buff, out x);
+            } while (x < -1 || !flag);
+        }
+
+        static void TestTask4()
+        {
+            int count = 0;
+            var testDict = new Dictionary<double, double>
+            {
+                [-16] = -1,
+                [-5] = -1,
+                [-2] = -1,
+                [-1.1] = -1,
+                [-1] = 1,
+                [-0.9] = 1.172,
+                [0] = 0,
+                [0.9] = 2.269,
+                [1] = 2.414,
+                [1.1] = 2.557,
+                [2] = 3.786,
+                [5] = 7.549,
+                [16] = 20.243
+            };
+
+
+            Console.WriteLine("\nСписок тестов");
+            Console.WriteLine("{0,7}{1,15}", "X", "Результат");
+
+            foreach (var i in testDict)
+            {
+                Console.WriteLine("{0,7}{1,15}", i.Key, i.Value);
+            }
+
+            //List<double> testSet = new List<double> { -5, -2, -1.1, -1, -0.9, 0, 0.9, 1, 1.1, 2, 5 };
+            Console.WriteLine();
+            Console.WriteLine("ТЕСТИРОВАНИЕ".PadLeft(23, '-').PadRight(35, '-'));
+            Console.WriteLine("\tX\tER\t   AR");
+            foreach (var i in testDict)
+            {
+                var x = i.Key;
+                var res = Task4(ref x);
+                res = Math.Round(res, 3);
+                if (res == i.Value) count++;
+
+                Console.WriteLine("{0,9}{1,9}{2,11}", i.Key, i.Value, res);
+
+            }
+            Console.WriteLine("\n\nРезультаты тестирования:\n\tВсего тестов:\t{0}\n\tУспешно:\t{1}\n\tПровалено:\t{2}", testDict.Count, count, (testDict.Count - count));
+
+        }
+        static double Task4(ref double x)
+        {
+
+            if (x < -1)
+            {
+                return -1;
+            }
+            else
+            {
+                var res = Math.Sqrt(x + Math.Sqrt(Math.Sqrt(Math.Abs(x)))) + Math.Abs(x);
+                return res;
+            }
+        }
+        static void Task()
+        {
+            float m = 0, n = 0;
+            double x = 0;
+            Read(ref m, ref n);
+            var res1 = n++ * m;
+            Console.WriteLine("\t1) Результат выражения \"(n++*m)\" - " + (res1) + ", тип выражения - " + res1.GetType());
+            var res2 = m-- < n;
+            Console.WriteLine("\t2) Результат выражения \"(m--<n)\" - " + (res2) + ", тип выражения - " + res2.GetType());
+            var res3 = ++m > n;
+            Console.WriteLine("\t3) Результат выражения \"(++m>n)\" - " + (res3) + ", тип выражения - " + res3.GetType());
+            Read_x(ref x);
+            var res4 = Task4(ref x);
+            Console.WriteLine("\t4) Результат выражения - " + res4 + " , тип выражения - " + res4.GetType());
+            TestTask4();
+        }
+    }
+}
---
+++ ООП (C#)/LR1/LR1/LR1_3/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Lab1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Task();
+        }
+
+
+        static void Equation(double a, double b)
+        {
+            double res1 = (Math.Pow((a + b), 4) - Math.Pow(a, 4)) / (6 * Math.Pow(a, 2) * Math.Pow(b, 2) + 4 * a * Math.Pow(b, 3) + Math.Pow(b, 4) + 4 * b * Math.Pow(a, 3));
+            double res2 = ((a + b) * (a + b) * (a + b) * (a + b) - a * a * a * a) / (6 * a * a * b * b + 4 * a * b * b * b + b * b * b * b + 4 * b * a * a * a);
+            Console.WriteLine("С использование Math.Pow: " + res1);
+            Console.WriteLine("Без использования Math.Pow: " + res2);
+        }
+
+        static void Equation(float a, float b)
+        {
+            float c = (float)(Math.Pow((a + b), 4));
+            float d = (float)(Math.Pow(a, 4));
+            float e = (float)(Math.Pow(a, 2));
+            float f = (float)(Math.Pow(b, 2));
+            float g = (float)(Math.Pow(b, 3));
+            float h = (float)(Math.Pow(b, 4));
+            float i = (float)(Math.Pow(a, 3));
+            float res1 = (float)(c - d) / (6 * e * f + 4 * a * g + h + 4 * b * i);
+            float res2 = (float)((a + b) * (a + b) * (a + b) * (a + b) - a * a * a * a) / (6 * a * a * b * b + 4 * a * b * b * b + b * b * b * b + 4 * b * a * a * a);
+            Console.WriteLine("С использование Math.Pow: " + res1);
+            Console.WriteLine("Без использования Math.Pow: " + res2);
+        }
+
+
+        static void Task()
+        {
+            double a = 1000d, b = 0.0001d;
+            float c = 1000f, d = 0.0001f;
+            Console.WriteLine("\nРезультат с типом double: ");
+            Equation(a, b);
+            Console.WriteLine("\nРезультат с типом float: ");
+            Equation(c, d);
+        }
+    }
+}
